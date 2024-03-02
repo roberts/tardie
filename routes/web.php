88b68940baaae42a3a531595f56d30aa5850b497
Drew Roberts <github@drewroberts.com>
2024-03-01 22:27:25 -0500

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('homepage');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/memes', function () {
+    return view('memes');
 });
