f3de8e5e05b15df0a9624ce9293309cad838a198
Drew Roberts <github@drewroberts.com>
2024-03-06 18:37:26 -0500

    Shorten margin at top of chart

--- resources/views/pages/chart.blade.php
+++ resources/views/pages/chart.blade.php
@@ -10,7 +10,7 @@
                             <h1 class="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">Robert Chart</h1>
                             <p class="mt-6 text-base text-zinc-600 dark:text-zinc-400">Here is the live $BOB chart on DexScreener:</p>
                         </header>
-                        <div class="mt-16 sm:mt-20">
+                        <div class="mt-6 sm:mt-6">
                             <style>#dexscreener-embed{position:relative;width:100%;padding-bottom:125%;}@media(min-width:1400px){#dexscreener-embed{padding-bottom:65%;}}#dexscreener-embed iframe{position:absolute;width:100%;height:100%;top:0;left:0;border:0;}</style>
                             <div id="dexscreener-embed">
                                 <iframe src="https://dexscreener.com/solana/CjwQrhXiLV4VWTcLsauyKbHNWCgmh1y9FBLtQezqCvCu?embed=1&theme=dark"></iframe>
