45f7f182759e8ef10d67008289343da32e6d62aa
Drew Roberts <github@drewroberts.com>
2024-03-05 21:43:40 -0500

    Insert twitter feed embed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,6 +42,9 @@
                     <div class="mx-auto max-w-2xl lg:max-w-5xl">
                         <div class="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
                             <div class="flex flex-col gap-16">
+                                <a class="twitter-timeline" data-lang="en" data-theme="dark" href="https://twitter.com/BobdaoSol?ref_src=twsrc%5Etfw">Tweets by BobdaoSol</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+                                <!-- Article link format for future
                                 <article class="group relative flex flex-col items-start">
                                     <h2 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
                                         <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
@@ -59,40 +62,8 @@
                                         </svg>
                                     </div>
                                 </article>
-                                <article class="group relative flex flex-col items-start">
-                                    <h2 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-                                        <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
-                                        <a href="/articles/crafting-a-design-system-for-a-multiplanetary-future">
-                                            <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                                            <span class="relative z-10">Another Sample Post</span>
-                                        </a>
-                                    </h2>
-                                    <time class="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5" datetime="2024-03-01"><span class="absolute inset-y-0 left-0 flex items-center" aria-hidden="true"><span class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span></span>March 1, 2024</time>
-                                    <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">This section here will be for highlighted social media posts or we could do blog articles. Only Bob knows what is best for it.</p>
-                                    <div aria-hidden="true" class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500">
-                                        View social post
-                                        <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="ml-1 h-4 w-4 stroke-current">
-                                            <path d="M6.75 5.75 9.25 8l-2.5 2.25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                        </svg>
-                                    </div>
-                                </article>
-                                <article class="group relative flex flex-col items-start">
-                                    <h2 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-                                        <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50"></div>
-                                        <a href="/articles/crafting-a-design-system-for-a-multiplanetary-future">
-                                            <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
-                                            <span class="relative z-10">Third Sample Post</span>
-                                        </a>
-                                    </h2>
-                                    <time class="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5" datetime="2024-03-01"><span class="absolute inset-y-0 left-0 flex items-center" aria-hidden="true"><span class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span></span>March 1, 2024</time>
-                                    <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">This section here will be for highlighted social media posts or we could do blog articles. Only Bob knows what is best for it.</p>
-                                    <div aria-hidden="true" class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500">
-                                        View social post
-                                        <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="ml-1 h-4 w-4 stroke-current">
-                                            <path d="M6.75 5.75 9.25 8l-2.5 2.25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                        </svg>
-                                    </div>
-                                </article>
+                                -->
+
                             </div>
                             <div class="space-y-10 lg:pl-16 xl:pl-24">
                                 <form class="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40" action="/thank-you">
