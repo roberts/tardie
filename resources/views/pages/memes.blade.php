89e81cdcc3f5201aac27dcc05fde40ac6ebf5199
Drew Roberts <github@drewroberts.com>
2024-03-06 18:09:29 -0500

    Update meme page title

--- resources/views/pages/memes.blade.php
+++ resources/views/pages/memes.blade.php
@@ -7,8 +7,8 @@
                 <div class="relative px-4 sm:px-8 lg:px-12">
                     <div class="mx-auto max-w-2xl lg:max-w-5xl">
                         <header class="max-w-2xl">
-                            <h1 class="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">$BOB Memes are coming soon..</h1>
-                            <p class="mt-6 text-base text-zinc-600 dark:text-zinc-400">We are all Bob. Bob's meme lords are working hard right now to deliver you the best memes in the history of Bob:</p>
+                            <h1 class="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">You can be anyBobby you wanna be</h1>
+                            <p class="mt-6 text-base text-zinc-600 dark:text-zinc-400">Feel free to use the best memes in the history of Bob wherever you want on the interwebs:</p>
                         </header>
                         {{--
                         <div class="mt-16 sm:mt-20">
