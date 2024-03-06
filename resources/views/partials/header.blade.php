999acb7603f1bf7397c96166bd8902374fdc0c06
Drew Roberts <github@drewroberts.com>
2024-03-06 18:07:12 -0500

    Add homepage to nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -47,6 +47,7 @@
                                 <nav x-show="open" class="pointer-events-auto hidden md:block">
                                     <ul class="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
                                         {{-- <li><a class="relative block px-3 py-2 transition text-teal-500 dark:text-teal-400" href="{{ route('about') }}">About<span class="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0"></span></a></li> --}}
+                                        <li><a class="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400" href="{{ route('home') }}">Home</a></li>
                                         <li><a class="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400" href="{{ route('about') }}">About</a></li>
                                         <li><a class="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400" href="/whitepaper.pdf">Whitepaper</a></li>
                                         <li><a class="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400" href="{{ route('memes') }}">Memes</a></li>
