b8f039e724e37404978f3c8b2bf18950f71990aa
Drew Roberts <github@drewroberts.com>
2024-03-06 19:34:31 -0500

    Add cloudinary config details

--- config/filesystems.php
+++ config/filesystems.php
@@ -30,6 +30,13 @@ return [
 
     'disks' => [
 
+        'cloudinary' => [
+            'driver' => 'cloudinary',
+            'api_key' => env('CLOUDINARY_API_KEY'),
+            'api_secret' => env('CLOUDINARY_API_SECRET'),
+            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        ],
+
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
