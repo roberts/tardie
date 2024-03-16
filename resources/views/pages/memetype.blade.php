c21a0f32be8987d92b868901618f2bbccf010f85
Drew Roberts <github@drewroberts.com>
2024-03-16 17:10:58 -0400

    Update meme page header styles

--- resources/views/pages/memetype.blade.php
+++ resources/views/pages/memetype.blade.php
@@ -6,7 +6,7 @@
             <div class="mx-auto w-full max-w-7xl lg:px-8">
                 <div class="relative px-4 sm:px-8 lg:px-12">
                     <div class="mx-auto max-w-2xl lg:max-w-5xl">
-                        <header class="max-w-2xl">
+                    <header>
                             <h1 class="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">You can be anyBobby you wanna be</h1>
                             <p class="mt-6 text-base text-zinc-600 dark:text-zinc-400">Feel free to use the best memes in the history of Bob wherever you want on the interwebs:</p>
                         </header>
